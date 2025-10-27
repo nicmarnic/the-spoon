d0a848fb4ff4fd1cfdb0c681af3415ec59ecf3ef
nicmarplay4-wq <nicmarplay4@gmail.com>
2025-10-27 04:23:26 +0100

    first mod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://thespoon-backend-production.up.railway.app/api'
+  //apiUrl: 'https://thespoon-backend-production.up.railway.app/api'
+  apiUrl: 'http://localhost:8080/api'
 };
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://thespoon-backend-production.up.railway.app/api'
+  //apiUrl: 'https://thespoon-backend-production.up.railway.app/api'
   //apiUrl: 'http://localhost:8080/api'
+  apiUrl: 'http://localhost:8080/api'
 };
